489b5cc7bbeff25679cc6c213f0ab9f17aa723bd
mi312 <107654821+ColorfulCookie@users.noreply.github.com>
2023-08-04 15:15:03 +0200

    cleanup repo

--- rtl/tb/uart_tb.vhd
+++ rtl/tb/uart_tb.vhd
@@ -10,7 +10,7 @@ architecture rtl of uart_tb is
         port (
             clk                      : in std_logic                     := '1';             --clock
             rst                      : in std_logic                     := '0';             --reset is high active
-            tx_data                  : out std_logic                    := '1';             --transmission bit output (Tx) (continuous 1 for 'off')
+            tx_data_out              : out std_logic                    := '1';             --transmission bit output (Tx) (continuous 1 for 'off')
             tx_ready                 : out std_logic                    := '0';             -- port for signaling begin ready to transmit a new data word
             tx_data_word             : out std_logic_vector(7 downto 0) := (others => '1'); --transmission word (Tx) (continuous 1 for 'off')
             tx_start                 : in std_logic                     := '0';
@@ -18,7 +18,7 @@ architecture rtl of uart_tb is
             rx_finished_out          : out std_logic                    := '0';                                  -- port for signaling having finished receiving a data word
             cfg_parity_setting       : in std_logic_vector(1 downto 0)  := "00";                                 -- 00 -> parity off, 01-> parity even, 10-> parity odd
             cfg_clkSpeed_over_bdRate : in std_logic_vector(31 downto 0) := std_logic_vector(to_unsigned(87, 32)) -- sets the effective baudrate; clk/bd, e.g. for a 10MHz clk and with 115.2k Bd: 10 000 000 / 115 200 = 86.8
-            -- bits_per_msg: integer := 8;--nr of data bits in each transmission; hardcoded to 8 
+            -- bits_per_word: integer := 8;--nr of data bits in each transmission; hardcoded to 8 
         );
     end component uart;
     constant c_bit_period             : time                          := 8.68 us; -- 115.2k Bd
@@ -33,15 +33,15 @@ architecture rtl of uart_tb is
     signal s_cfg_parity_setting       : std_logic_vector(1 downto 0)  := "00";
     signal s_cfg_clkSpeed_over_bdRate : std_logic_vector(31 downto 0) := std_logic_vector(to_unsigned(87, 32));
 
-    procedure send_uart_data(data : in std_logic_vector(7 downto 0); signal s_UART_RX : out std_logic) is
+    procedure send_uart_data(s_rx_test_data : in std_logic_vector(7 downto 0); signal s_rx_data : out std_logic) is
     begin
-        s_UART_RX <= '0';
+        s_rx_data <= '0';
         wait for c_bit_period;
         for i in 0 to 7 loop
-            s_UART_RX <= data(i);
+            s_rx_data <= s_rx_test_data(i);
             wait for c_bit_period;
         end loop;
-        s_UART_RX <= '1';
+        s_rx_data <= '1';
         wait for c_bit_period;
     end procedure;
 
@@ -51,7 +51,7 @@ begin
     port map(
         clk                      => s_clk,
         rst                      => s_rst,
-        tx_data                  => s_tx_data,
+        tx_data_out              => s_tx_data,
         tx_ready                 => s_tx_ready,
         tx_data_word             => s_tx_data_word,
         tx_start                 => s_tx_start,
@@ -60,6 +60,7 @@ begin
         cfg_parity_setting       => s_cfg_parity_setting,
         cfg_clkSpeed_over_bdRate => s_cfg_clkSpeed_over_bdRate
     );
+
     process
     begin
         wait for 100 ns;
