873b4be529c99779dec277aacbe7dd2eaffb8ec7
ColorfulCookie <107654821+ColorfulCookie@users.noreply.github.com>
2023-08-04 20:51:26 +0200

    Update fgile naming in verilog_conversion.sh

--- scripts/verilog_conversion.sh
+++ scripts/verilog_conversion.sh
@@ -1,5 +1,5 @@
 
 ghdl -a --std=08 uart_clock.vhd
-ghdl -a --std=08 uart.vhd
+ghdl -a --std=08 uartus.vhd
 #ghdl synth --std=08 --out=verilog uart_clock > uart_clock.v
-ghdl synth --std=08 --out=verilog uart > uart.v
+ghdl synth --std=08 --out=verilog uartus > uart.v
